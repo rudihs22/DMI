51e96bd4d273dbd18746fe70760cdfa694880229
Guest <rudihs22@inbox.lv>
2017-09-28 09:51:23 +0300

    20170928_09_50

---
+++ shell_examples.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#5.piemērs
+
+#4.piemērs
+echo "atbilde uz jautajumu kas šobrid tiek pildīts" $0
+#echo $n
+echo  "skriptam nodoto argumentu skaits" $#
+
+#3. piemērs
+#NAME="Vaards Uzvaards"
+#echo $NAME
+#unset NAME
+#echo $NAME
+
+
+#2. piemērs
+#NAME="Vaards Uzvaards"
+#readonly NAME
+#echo $NAME
+#Name="Uzvaards Vards"
+#echo $NAME
+
+
+#1. piemērs
+#NAME="Vaards Uzvaards"
+#echo $NAME
