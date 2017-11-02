81022ea6f0bed0c81fbe5adc4c5ce7578db6ea28
rastamaha1998@gmail.com <rudihs22@inbox.lv>
2017-11-02 11:49:42 +0200

    02.11.2017

---
+++ PYTHON/hello.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+#http://www.tutorialspoint.com/unix_commands/echo.htm
+
+#print "Hello World"
+'''
+#Example 3.
+print "Here \bthe \bspaces \bare \bbackspaced."
+'''
+'''
+#Example 4.
+print "Here \nthe \nspaces \nare \nnewlined."
+'''
+'''
+#Example 5.
+print "Here \tthe \tspaces \thave \thorizontal \ttab \tspaces."
+'''
+
+#Example 6.
+print "Nav \vman \vdzejola \vtapec \vrakstu \vparastu \vteikumu \n				kam \n			nav \n		sakara \n	ar so \ndzejoli "
+#print "kam nav nekada sakara ar skaistu dzejoli"
+
+#echo(Shell) = print(python)!!
