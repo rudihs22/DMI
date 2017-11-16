e60a79781047b457f880a7c842048dd5ff846a30
rastamaha1998@gmail.com <rudihs22@inbox.lv>
2017-11-16 11:48:25 +0200

    16112017

---
+++ PYTHON/if.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+a = input("Lietotāj, lūdzu, ievadi skaitli: ")
+
+if a > 0:
+	print "Tu esi ievadijis pozitīvu skaitli"
+elif a == 0:
+	print "Tu esi ievadijis Nulli"
+else:
+	print "Tu esi ievadījis negativu skaitli"
+print "Bet šo tektu Tu redzesi katru reizi!" 
---
+++ PYTHON/while.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+a = input("Lietotāj, lūdzu, ievadi beigu vērtību: ")
+
+k = 1
+while k <= a:
+    print "Mainīgā k kārtējā vērtība ir: %d\n"%(k),
+    k = k + 1
+print "Beigas!"
