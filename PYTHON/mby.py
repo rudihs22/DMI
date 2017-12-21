a9674f4c4c61b24928d26e46fcd9d5ba78bc6772
rastamaha1998@gmail.com <rudihs22@inbox.lv>
2017-12-21 09:44:33 +0200

    20171221-9.51

---
+++ PYTHON/mby.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import math
+
+x = int(input("Lietotaj, ludzu ievadi skaitli: "))
+print "Dalijumu atlikumu vertibas tabula".center(80)
+print " \t1 \t2 \t3 \t4 \t5 \t6 \t7 \t8 \t9"
+print x
+for i in range(x, 11):
+   print ()
